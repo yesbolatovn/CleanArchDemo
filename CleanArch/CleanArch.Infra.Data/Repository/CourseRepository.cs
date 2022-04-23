884fa1d4570c94027caa108a51e715fe13aeb254
Nursultan Yesbolatov <nyem@tengizchevroil.com>
2022-04-23 17:29:11 +0500

    Commands, CommandHandlers, Updated CourseRepository and Cleaned up IoC Container

--- CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -15,6 +15,13 @@ namespace CleanArch.Infra.Data.Repository
         {
             _ctx = ctx;
         }
+
+        public void Add(Course course)
+        {
+            _ctx.Courses.Add(course);
+            _ctx.SaveChanges();
+        }
+
         public IEnumerable<Course> GetCourses()
         {
             return _ctx.Courses;
